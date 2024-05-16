b699f75fe756e15ba04f14938427118885b5e196
Pati <karol.hamuda@gmail.com>
2024-05-16 16:54:11 +0200

    About section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as styles from "./about.module.css";
+import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 const About = () => {
   const [showPictures, setShowPictures] = React.useState(false);
@@ -23,25 +24,26 @@ const About = () => {
             Creative minds crafting distinctive symbols, shaping brands, and constructing identity systems tailored to individuals.
           </div>
           <div className={`${styles.aboutButtonContainer} ${isHovering ? styles.hovering : ""}`}>
-            <div className={styles.aboutButton} >
+            <AnchorLink to="/#aboutus" className={styles.aboutButton} >
               ABOUT US
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-              <circle cx="12" cy="12" r="12" fill="#EDFF23"/>
-              <path d="M13.9304 15.9517L13.1634 15.1889L15.8267 12.5298H6V11.4219H15.8267L13.1634 8.75852L13.9304 8L17.9062 11.9759L13.9304 15.9517Z" fill="black"/>
+            </AnchorLink>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+              <circle cx="12.5" cy="12" r="12" fill="#EDFF23" className={styles.circle}/>
+              <path d="M14.4304 15.9517L13.6634 15.1889L16.3267 12.5298H6.5V11.4219H16.3267L13.6634 8.75852L14.4304 8L18.4062 11.9759L14.4304 15.9517Z" fill="black" className={styles.arrow}/>
+              <path d="M14.4304 15.9517L13.6634 15.1889L16.3267 12.5298H6.5V11.4219H16.3267L13.6634 8.75852L14.4304 8L18.4062 11.9759L14.4304 15.9517Z" fill="black" className={styles.hiddenArrow}/>
             </svg>
           </div>
         </div>
         <div className={`${styles.introduction} ${isHovering ? styles.hovering : ""}`}>
-          Maverick Studio, located in Poland, is your go-to creative expert, dedicated to crafting unique{" "}
+        We are your creative expert, dedicated to crafting unique{" "}
           <div
             className={styles.underline}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
           >
-            brands and logos
+            brands
           </div>{" "}
-          for early-stage startups and small to medium-sized brands.
+          for startups and small to medium-sized brands.
 
         </div>
         {showPictures && (
