557a71533fa9d097e42e8f1870ce2fce3a71f374
Pati <karol.hamuda@gmail.com>
2024-05-28 14:42:21 +0200

    Image scroll

--- src/components/slideshow/slideshow.js
+++ src/components/slideshow/slideshow.js
@@ -1,42 +1,40 @@
-import React from 'react'
-import * as styles from "./slideshow.module.css"
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import * as styles from './slideshow.module.css';
+import sliderImage from '../../images/slider.jpeg';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Slideshow = () => {
+  useEffect(() => {
+    const tl = gsap.timeline({ ease: 'none' });
+
+    tl.from(`.${styles.image}`, {
+      scale: 0.6,
+      duration: 1,
+      transformOrigin: 'top',
+    }).to({}, { duration: 1 });
+
+    ScrollTrigger.create({
+      trigger: `.${styles.imageContainer}`,
+      animation: tl,
+      start: "top 80%",
+      end: "top 0%",
+      scrub: true,
+      toggleActions: "restart none none none",
+
+    });
+  }, []);
+
   return (
-  <>
+    <>
     <div className={styles.imageContainer}>
-      <div className={styles.smallLeftImage}>
-
-      </div>
-      <div className={styles.Image}>
-
-      </div>
-      <div className={styles.smallRightImage}>
-
-      </div>
+      <img className={styles.image} src={sliderImage} alt="Slideshow" />
     </div>
-    <div className={styles.linkContainer}>
-      <div className={styles.text}>
-        FLOWHUB MAUI
-      </div>
-      <div className={styles.textLink}>
-        VISUAL IDENTITY
-      </div>
-      <div className={styles.textLink}>
-        CREATIVE DIRECTION
-      </div>
-      <div className={styles.textLink}>
-        LOGO DESIGN
-      </div>
-      <div className={styles.pointerLink}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <circle cx="12" cy="12" r="12" fill="#EDFF23"/>
-        <path d="M13.9304 15.9517L13.1634 15.1889L15.8267 12.5298H6V11.4219H15.8267L13.1634 8.75852L13.9304 8L17.9062 11.9759L13.9304 15.9517Z" fill="black"/>
-      </svg>
-      </div>
-    </div>
+    <div className={styles.rectangle} />
     </>
-  )
-}
+  );
+};
 
-export default Slideshow
+export default Slideshow;
