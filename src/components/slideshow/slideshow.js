a6762add4815f8c863feb83808f48f6bc8e01394
Pati <karol.hamuda@gmail.com>
2024-06-06 13:22:28 +0200

    slideshow update

--- src/components/slideshow/slideshow.js
+++ src/components/slideshow/slideshow.js
@@ -1,40 +1,17 @@
 import React, { useEffect } from 'react';
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
+
 import * as styles from './slideshow.module.css';
 import sliderImage from '../../images/slider.jpeg';
 
-gsap.registerPlugin(ScrollTrigger);
+
 
 const Slideshow = () => {
-  useEffect(() => {
-    const tl = gsap.timeline({ ease: 'none' });
-
-    tl.from(
-      `.${styles.image}`, 
-    {
-      scale: 0.3,
-      duration: 1,
-      transformOrigin: 'top',
-    }
-    ).to({}, { duration: 1 });
-
-    ScrollTrigger.create({
-      trigger: `.${styles.imageContainer}`,
-      animation: tl,
-      start: "top 80%",
-      end: "top -50%",
-      scrub: 1,
-      toggleActions: "restart none none none",
-      
-    });
-  }, []);
 
   return (
     <>
     <div className={styles.imageContainer}>
       <img className={styles.image} src={sliderImage} alt="Slideshow" />
-      <div className={styles.rectangle} />
+      
     </div>
     
     </>
